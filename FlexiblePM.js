fe1e34f149ca4620402e4605b1cbbc32be7d16e5
Jaime Escalona <31493381+jaescalo@users.noreply.github.com>
2022-04-07 15:00:00 -0600

    Update FlexiblePM.js

--- FlexiblePM.js
+++ FlexiblePM.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name PM Flexible UI
+// @name PM Flexible UI - Dark Reader Compatible
 // @namespace ''
 // @description Make the Property Manager UI more flexible
 // @match https://control.akamai.com/apps/property-manager/*
@@ -20,9 +20,6 @@ function addGlobalStyle(css) {
 // Change background
 addGlobalStyle ('body.core-1-7, body.core-1-8 { background: rgb(21,42,74); background: radial-gradient(circle, rgba(21,42,74,1) 0%, rgba(25,63,103,1) 35%, rgba(51,153,203,1) 100%); }');
 
-// Texts color
-addGlobalStyle ('body { color: #424554; }');
-
 // Title Color
 addGlobalStyle ('h1, h3, .h1 ,h3 { color: #FFF; }')
 
@@ -30,12 +27,10 @@ addGlobalStyle ('h1, h3, .h1 ,h3 { color: #FFF; }')
 addGlobalStyle('.container { width: 85% !important; }');
 
 // Adjust the variable list box to fit all variables
-addGlobalStyle('pm-variable-list .variable-table-scrolling-wrapper { max-height: 500px !important; }');
+addGlobalStyle('pm-variable-list .variable-table-scrolling-wrapper { max-height: 400px !important; }');
 
 // Set the rule tree height, width and color
-//addGlobalStyle ('.rule-tree .scroll { height: 100%; overflow-y: visible; overflow-x: visible; }');
 addGlobalStyle ('.col-xs-3 { width: 35%; }');
-addGlobalStyle ('.rule-tree .list-group>.list-group-item { background-color: rgb(254 255 245); }');
 
 // Set the matches and behaviors panel width
 addGlobalStyle ('.col-xs-9 { width: 65%; }');
@@ -43,13 +38,6 @@ addGlobalStyle ('.col-xs-9 { width: 65%; }');
 // Set the rules box height and scroll
 addGlobalStyle ('.rules { height:1500px; overflow-y:auto; }');
 
-// Change criteria box color
-addGlobalStyle ('.edit-page pm-match-list .matches.all, .edit-page pm-match-list .matches.any .pm-match { background-color: #f7ebe2; }');
-addGlobalStyle ('.edit-page .no-matches, .edit-page .pm-match { background-color: #f7ebe2 }');
-addGlobalStyle ('.edit-page pm-match-list .matches .condition-separator > span { background: #f7ebe2 }');
-
-// Change the behavior box color
-addGlobalStyle ('.edit-page .no-behaviors, .edit-page pm-behavior { background-color: #d5ebf4; }');
 
 // Move Info box to the right side and re-color
 addGlobalStyle ('.popover { left: unset !important; right: 0 !important; background-color: rgb(189 255 197); }');
